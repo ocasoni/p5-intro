a7b9eea9661d13e4a5e4243923e7536ab3116b9f
ocasoni <o.casoni@ied.edu>
2026-03-26 16:14:38 +0100

    images collage

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-  createCanvas(400, 400);
+let tramonto;
+
+function preload() {
+  tramonto = loadImage("image.jpg");
 }
 
-function draw() {
-  background(220);
+
+function setup() {
+  createCanvas(400, 400);
+  background(160,0,0);
+
+  let x;
+  let y;
+
+
+  for (let i=0; i<100; i = i + 1) {
+    x = random(300);
+    y = random(300);
+    image(tramonto, x, y, 50, 50);
+  }
 }
